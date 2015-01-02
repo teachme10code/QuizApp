39a92710f63465c107b9f5a58342be7fa886b918
chris ward <chris@chriss-MacBook-Pro.local>
2015-01-02 13:59:23 -0800

    Tracking question number Changed some styling

--- js/app.js
+++ js/app.js
@@ -19,9 +19,13 @@ function changeAnswerSet() {
 	}
 }
 
-function displayQuestion() {
-	var questions = ["1. What is the most popular breed of dog?", "2. What is the most popular male dog name?", "3. What is the most popular female dog name?", "4. Which dog breed has a black tongue?", "5. What is the name of the phobia for someone who has a fear of dogs?"];
+function displayQuestionStuff() {
+	var questions = ["What is the most popular breed of dog?", "What is the most popular male dog name?", "3. What is the most popular female dog name?", "4. Which dog breed has a black tongue?", "5. What is the name of the phobia for someone who has a fear of dogs?"];
+	var questionNumber = n + " of " + questions.length;
 	var question = questions[i];
+	if (n <= questions.length) {
+		$('#questionNumber').text("Question " + questionNumber);
+	}
 	$('#question').text(question);
 	return
 }
@@ -37,6 +41,6 @@ function countCorrectAnswers() {
 }
 
 function displayFunFact() {
-	var factList = ["","Fact: Labradors are athletic, playful, and the most popular breed of dog by registered ownership in Australia, Canada, New Zealand, the United Kingdom, and the United States (since 1991).", "Fact: After Max, the names Charlie, Buddy and Jack are the next most popular names for male dogs.", "Fact: The name Bella is the most popular, with Lucy, Bailey and Daisy as the following top choices for female dogs.", "Fact: The blue-black/purple tongue gene appears to be dominant, as most mixed breed dogs that come from a Chow Chow retain that tongue color.", "Fact: Agrizoophobia is a fear of wild animals, Barophobia is the irrational fear of gravity, and Ranidaphobia is the fear of frogs and toads."]
+	var factList = ["","* Labradors are athletic, playful, and the most popular breed of dog by registered ownership in Australia, Canada, New Zealand, the United Kingdom, and the United States (since 1991).", "* After Max, the names Charlie, Buddy and Jack are the next most popular names for male dogs.", "* The name Bella is the most popular, with Lucy, Bailey and Daisy as the following top choices for female dogs.", "* The blue-black/purple tongue gene appears to be dominant, as most mixed breed dogs that come from a Chow Chow retain that tongue color.", "* Agrizoophobia is a fear of wild animals, Barophobia is the irrational fear of gravity, and Ranidaphobia is the fear of frogs and toads."]
 	$("#funFact").text(factList[i]);
 }
