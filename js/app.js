2cc49ef664ae3ab32c6a21326e98515a65405dc7
chris ward <chris@chriss-MacBook-Pro.local>
2015-01-04 16:22:26 -0800

    remainder of commit

--- js/app.js
+++ js/app.js
@@ -1,6 +1,21 @@
 var i = 0;
 var n = 1;
 var numRight = 0;
+var is_checked = true;
+
+function validateForm() {
+	$('input').each(function() {
+		is_checked = $(this).is(':checked');
+		if (is_checked) {
+			incrementIndex();
+			changeAnswerSet();
+			displayQuestionStuff();
+			countCorrectAnswers();
+			displayFunFact();
+		}
+		return
+	});
+}
 
 function incrementIndex(){
 	i++;
@@ -12,35 +27,41 @@ function changeAnswerSet() {
 	var currentAnswerSet = ".form_container"+i;
 	var nextAnswerSet = ".form_container"+n;
 	$(nextAnswerSet).css("visibility", "visible");
-	if(n<=5){
-		$(currentAnswerSet).css("visibility", "hidden");
-		} else {
-		return
+	$(currentAnswerSet).css("visibility", "hidden");
+	if(n>5){
+		displayFinalResults();
 	}
 }
 
 function displayQuestionStuff() {
-	var questions = ["What is the most popular breed of dog?", "What is the most popular male dog name?", "3. What is the most popular female dog name?", "4. Which dog breed has a black tongue?", "5. What is the name of the phobia for someone who has a fear of dogs?"];
+	var questions = ["What is the most popular breed of dog?", "What is the most popular male dog name?", "What is the most popular female dog name?", "Which dog breed has a black tongue?", "What is the name of the phobia for someone who has a fear of dogs?"];
 	var questionNumber = n + " of " + questions.length;
 	var question = questions[i];
 	if (n <= questions.length) {
 		$('#questionNumber').text("Question " + questionNumber);
 	}
 	$('#question').text(question);
-	return
+	return 
 }
 
 function countCorrectAnswers() {
 	var answer = $(".list").find("input[name=answer]:checked").val();
-	if(answer === "right") {
+	console.log(answer);
+	if(answer === "right" && $('input').is(':checked')) {
 		numRight++;
-		console.log(numRight);
-	} else {
-		return
 	}
+	return
 }
 
 function displayFunFact() {
 	var factList = ["","* Labradors are athletic, playful, and the most popular breed of dog by registered ownership in Australia, Canada, New Zealand, the United Kingdom, and the United States (since 1991).", "* After Max, the names Charlie, Buddy and Jack are the next most popular names for male dogs.", "* The name Bella is the most popular, with Lucy, Bailey and Daisy as the following top choices for female dogs.", "* The blue-black/purple tongue gene appears to be dominant, as most mixed breed dogs that come from a Chow Chow retain that tongue color.", "* Agrizoophobia is a fear of wild animals, Barophobia is the irrational fear of gravity, and Ranidaphobia is the fear of frogs and toads."]
 	$("#funFact").text(factList[i]);
+}
+
+function displayFinalResults() {
+	$('#questionNumber').css("visibility", "hidden");
+	$('#question').css("visibility", "hidden");
+	$('#submit').css("visibility", "hidden");
+	$('#finalResults').css("visibility", "visible");
+	$('#finalResults').text("You got " + numRight + " out of " + i + " correct!");
 }
